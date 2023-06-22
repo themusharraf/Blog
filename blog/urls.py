db1edfff9036ba9787d847c10620a55a10348f18
themusharraf <meibrohimov@gmail.com>
2023-06-22 20:47:14 +0500

    add favoriting api full commit ✅

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from blog.views import PostModeViewSet, CommentListViewSet
+from blog.views import PostModeViewSet, CommentListViewSet, ProductFavoriteView
 
 router = DefaultRouter()
 router.register(r'blog', PostModeViewSet)
@@ -8,4 +8,6 @@ router.register(r'blog', PostModeViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('comment', CommentListViewSet.as_view(), name='comments'),
+    path('post/<int:pk>/favorite/', ProductFavoriteView.as_view(), name='product_favorite'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
-from blog.models import Post, Comment
-from blog.serializers import PostSerializer, PostDetailModelSerializer, CommentModelSerializer
-from rest_framework.response import Response
-from blog.permissions import IsAuthorOrReadOnly
+from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from blog.models import Comment
+from blog.models import Post
+from blog.permissions import IsAuthorOrReadOnly
+from blog.serializers import PostDetailModelSerializer
+from blog.serializers import PostSerializer, CommentModelSerializer
 
 
 class PostModeViewSet(ModelViewSet):
@@ -24,3 +27,22 @@ class CommentListViewSet(ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentModelSerializer
     permission_classes = [IsAuthorOrReadOnly]
+
+
+class ProductFavoriteView(generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailModelSerializer
+
+    def post(self, request, pk):
+        product = self.get_object()
+        product.is_favorite = True
+        product.save()
+        serializer = self.get_serializer(product)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        product = self.get_object()
+        product.is_favorite = False
+        product.save()
+        serializer = self.get_serializer(product)
+        return Response(serializer.data)
