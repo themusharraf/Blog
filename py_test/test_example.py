e3fd22bf47c1f21fcf6a1902b0dbee7c570b8b50
msi <meibrohimov@gmail.com>
2023-08-04 11:33:12 +0500

    Pytest test_example.py in model test 👽

--- py_test/test_example.py
+++ py_test/test_example.py
@@ -6,3 +6,29 @@ def test_user(db, user_factory):
     user = user_factory.create()
     print(user.username)
     assert True
+
+
+@pytest.mark.django_db
+def test_category(db, category_factory):
+    category = category_factory.create()
+    print(category.name)
+    assert True
+
+
+@pytest.mark.django_db
+def test_post(db, post_factory, user_factory, category_factory):
+    category = category_factory.create()
+    user = user_factory.create()
+    post = post_factory.create(author=user, category=category)
+    print(post.title)
+    assert True
+
+
+@pytest.mark.django_db
+def test_comment(db, comment_factory, user_factory, post_factory, category_factory):
+    user = user_factory.create()
+    category = category_factory.create()
+    post = post_factory.create(author=user, category=category)
+    comment = comment_factory.create(owner=user, post=post)
+    print(comment.post)
+    assert True
